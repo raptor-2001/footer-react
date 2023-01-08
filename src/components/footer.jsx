30464be4b08562cfc9890d7dcbf130803b22f74b
raptor-2001 <pratham007yadav@gmail.com>
2023-01-08 14:24:33 +0530

    footer updated

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -8,34 +8,33 @@ const footer = () => {
         
         <ul class="footer-list-top">
           <li>
-            <h4 class="footer-list-header">About Pavilion</h4></li>
+            <h4 class="footer-list-header">About Pragati</h4></li>
           <li><a href='/shop/about-mission' class="generic-anchor footer-list-anchor" itemprop="significantLink">GET TO KNOW US</a></li>
-          <li><a href='/promos.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">PROMOS</a></li>
-          <li><a href='/retailers/new-retailers.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">BECOME A RETAILER</a></li>
+          <li><a href='/promos.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">JOBS</a></li>
+          <li><a href='/retailers/new-retailers.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">SCHOLARSHIP</a></li>
 
-          <li><a href='/job-openings.html' itemprop="significantLink" class="generic-anchor footer-list-anchor">JOB OPENINGS</a></li>
+          <li><a href='/job-openings.html' itemprop="significantLink" class="generic-anchor footer-list-anchor">COURSES</a></li>
 
-          <li><a href='/shop/about-show-schedule' class="generic-anchor footer-list-anchor" itemprop="significantLink">EVENTS</a></li>
         </ul>
         <ul class="footer-list-top">
           <li>
-            <h4 class="footer-list-header">The Gift Selection</h4></li>
+            <h4 class="footer-list-header">Our Motive</h4></li>
 
 
-          <li><a href='/Angels/cat/id/70' class="generic-anchor footer-list-anchor">ANGEL FIGURINES</a></li>
-          <li><a href='/Home-Decor/cat/id/64' class="generic-anchor footer-list-anchor">HOME DECOR</a></li>
-          <li><a href='/Mugs/cat/id/32' class="generic-anchor footer-list-anchor">MUGS</a></li>
-          <li><a href='/Pet-Lover/cat/id/108' class="generic-anchor footer-list-anchor">PET LOVER</a></li>
-          <li><a href='/Ladies-Accessories/cat/id/117' class="generic-anchor footer-list-anchor" target="_blank">HANDBAGS & JEWELRY</a></li>
+          <li><a href='/Angels/cat/id/70' class="generic-anchor footer-list-anchor">WOMEN EMPOWERMENT</a></li>
+          <li><a href='/Home-Decor/cat/id/64' class="generic-anchor footer-list-anchor">LEARN SKILLS</a></li>
+          <li><a href='/Mugs/cat/id/32' class="generic-anchor footer-list-anchor">JOB OPPURTUNTIES</a></li>
+          <li><a href='/Pet-Lover/cat/id/108' class="generic-anchor footer-list-anchor">FINANCE</a></li>
+          <li><a href='/Ladies-Accessories/cat/id/117' class="generic-anchor footer-list-anchor" target="_blank"></a></li>
         </ul>
         <ul class="footer-list-top">
           <li id='help'>
-            <h4 class="footer-list-header">Please Help Me</h4></li>
+            <h4 class="footer-list-header">Connect</h4></li>
           <li><a href='/shop/about-contact' class="generic-anchor footer-list-anchor" itemprop="significantLink">CONTACT</a></li>
           <li><a href='/faq.html' class="generic-anchor footer-list-anchor" itemprop="significantLink">FAQ</a></li>
-          <li id='find-a-store'><a href='/shop/store-locator' class="generic-anchor footer-list-anchor" itemprop="significantLink">STORE LOCATOR</a></li>
-          <li id='user-registration'><a href='/shop/user-registration?URL=' class="generic-anchor footer-list-anchor" itemprop="significantLink">NEW USERS</a></li>
-          <li id='order-tracking'><a href='/shop/order-status' itemprop="significantLink" class="generic-anchor footer-list-anchor">ORDER STATUS</a></li>
+          <li id='find-a-store'><a href='/shop/store-locator' class="generic-anchor footer-list-anchor" itemprop="significantLink">SUPPORT</a></li>
+          <li id='user-registration'><a href='/shop/user-registration?URL=' class="generic-anchor footer-list-anchor" itemprop="significantLink">COMMUNITY</a></li>
+          <li id='order-tracking'><a href='/shop/order-status' itemprop="significantLink" class="generic-anchor footer-list-anchor"></a></li>
         </ul>
         <section class="footer-social-section flex-rw">
             <span class="footer-social-overlap footer-social-connect">
@@ -54,7 +53,7 @@ const footer = () => {
       <div class="footer-bottom-wrapper">   
       <i class="fa fa-copyright" role="copyright">
       
-      </i> 2019 Pavilion in <address class="footer-address" role="company address">Bergen, NY</address><span class="footer-bottom-rights"> - All Rights Reserved - </span>
+      </i> 2023  Pragati in <address class="footer-address" role="company address">MS, India</address><span class="footer-bottom-rights"> - All Rights Reserved - </span>
           </div>
           <div class="footer-bottom-wrapper">
           <a href="/terms-of-use.html" class="generic-anchor" rel="nofollow">Terms</a> | <a href="/privacy-policy.html" class="generic-anchor" rel="nofollow">Privacy</a>
